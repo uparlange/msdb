a46018c8c8567c03a58b8b492c09f7ae4c477a5e
parlangeu <ugo.parlange@cgi.com>
2016-12-14 18:56:03 +0100

    Try jsdoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,20 @@
 module.exports = function (grunt) {
 
 	var pkg = grunt.file.readJSON('package.json');
-	
-    grunt.initConfig({
-		pkg:pkg,
-        clean: {
-            dist: ['dist'],
+
+	grunt.initConfig({
+		pkg: pkg,
+		clean: {
+			dist: ['dist'],
 			babel: ['dist/js/*.babel.js'],
 			index_temp: ['dist/index_temp.html']
-        },
+		},
 		jshint: {
 			options: {
 				esversion: 6
 			},
 			classes: ['js/*.js']
-        },
+		},
 		babel: {
 			options: {
 				sourceMap: false
@@ -29,8 +29,8 @@ module.exports = function (grunt) {
 				}]
 			}
 		},
-        uglify: {
-            classes: {
+		uglify: {
+			classes: {
 				files: [{
 					expand: true,
 					cwd: 'dist/js/',
@@ -38,32 +38,32 @@ module.exports = function (grunt) {
 					dest: 'dist/js/',
 					ext: '.js'
 				}]
-            }
-        },
-        htmlmin: {
+			}
+		},
+		htmlmin: {
 			options: {
-				caseSensitive:true,
+				caseSensitive: true,
 				removeComments: true,
 				collapseWhitespace: true
 			},
-			index_temp:{
+			index_temp: {
 				src: 'dist/index_temp.html',
-                dest: 'dist/index.html'
+				dest: 'dist/index.html'
 			},
-            templates: {
-                files: [{
-                    expand: true,
-                    cwd: 'html',
-                    src: '**/*.html',
-                    dest: 'dist/html'
-                }]
-            }
-        },
-        cssmin: {
-            options: {
-                keepSpecialComments: 0,
-				rebase:false
-            },
+			templates: {
+				files: [{
+					expand: true,
+					cwd: 'html',
+					src: '**/*.html',
+					dest: 'dist/html'
+				}]
+			}
+		},
+		cssmin: {
+			options: {
+				keepSpecialComments: 0,
+				rebase: false
+			},
 			css: {
 				files: [{
 					expand: true,
@@ -73,44 +73,43 @@ module.exports = function (grunt) {
 					ext: '.css'
 				}]
 			}
-        },
+		},
 		copy: {
 			package: {
-				src:'package.json',
-				dest:'dist/package.json'
+				src: 'package.json',
+				dest: 'dist/package.json'
 			},
 			material_design_icons: {
-                cwd: 'node_modules/material-design-icons/iconfont',
-                src: '**/*',
-                dest: 'dist/node_modules/material-design-icons/iconfont',
-                expand: true
-            },
+				cwd: 'node_modules/material-design-icons/iconfont',
+				src: '**/*',
+				dest: 'dist/node_modules/material-design-icons/iconfont',
+				expand: true
+			},
 			photoswipe_default_skin: {
-                cwd: 'node_modules/photoswipe/dist/default-skin',
-                src: '**/*',
-                dest: 'dist/node_modules/photoswipe/dist/default-skin',
-                expand: true
-            },
+				cwd: 'node_modules/photoswipe/dist/default-skin',
+				src: '**/*',
+				dest: 'dist/node_modules/photoswipe/dist/default-skin',
+				expand: true
+			},
 			data: {
 				cwd: 'data',
-                src: '**/*',
-                dest: 'dist/data',
-                expand: true
+				src: '**/*',
+				dest: 'dist/data',
+				expand: true
 			},
 			index: {
-				src:'index.html',
-				dest:'dist/index_temp.html',
+				src: 'index.html',
+				dest: 'dist/index_temp.html',
 				options: {
-                    process: function (content, srcpath)
-                    {
-                        return content.replace(/APP_VERSION/g, pkg.version);
-                    }
-                }
+					process: function (content, srcpath) {
+						return content.replace(/APP_VERSION/g, pkg.version);
+					}
+				}
 			}
-        },
+		},
 		imagemin: {
 			options: {
-				optimizationLevel : 3
+				optimizationLevel: 3
 			},
 			dynamic: {
 				files: [{
@@ -123,18 +122,26 @@ module.exports = function (grunt) {
 		},
 		sass: {
 			options: {
-				sourcemap:"none"
+				sourcemap: "none"
 			},
 			theme: {
 				files: {
 					'css/theme.css': 'theme/theme.scss'
 				}
 			}
+		},
+		jsdoc: {
+			doc: {
+				src: ['README.md'],
+				options: {
+					destination: 'doc'
+				}
+			}
 		}
-    });
+	});
 
 	grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -142,89 +149,79 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	
-	grunt.registerTask('prepare', 'prepare', function()
-	{
+	grunt.loadNpmTasks('grunt-jsdoc');
+
+	grunt.registerTask('prepare', 'prepare', function () {
 		const htmlparser = require('htmlparser2');
 		const fs = require('fs');
-		
+
 		const uglify = {};
 		const cssmin = {};
 		const copy = {};
 		const parser = new htmlparser.Parser(
-		{
-			onopentag: function(tagname, attributes)
 			{
-				let attribute = null;
-				let dependencies = null;
-				switch(tagname)
-				{
-					case 'script' : 
-						attribute = 'src'; 
-						dependencies = uglify;
-						break;
-					case 'link' : 
-						attribute = 'href'; 
-						dependencies = cssmin;
-						break;
-				}
-				if(attributes[attribute] !== undefined && attributes[attribute].indexOf('node_modules') !== -1)
-				{
-					const url = attributes[attribute];
-					const begin = url.lastIndexOf("/") + 1;
-					const name = url.substring(begin);
-					if(name.indexOf('.min') !== -1)
-					{
-						dependencies = copy;
+				onopentag: function (tagname, attributes) {
+					let attribute = null;
+					let dependencies = null;
+					switch (tagname) {
+						case 'script':
+							attribute = 'src';
+							dependencies = uglify;
+							break;
+						case 'link':
+							attribute = 'href';
+							dependencies = cssmin;
+							break;
 					}
-					dependencies[name] = {
-						src:url,
-						dest:'dist/'+url
+					if (attributes[attribute] !== undefined && attributes[attribute].indexOf('node_modules') !== -1) {
+						const url = attributes[attribute];
+						const begin = url.lastIndexOf("/") + 1;
+						const name = url.substring(begin);
+						if (name.indexOf('.min') !== -1) {
+							dependencies = copy;
+						}
+						dependencies[name] = {
+							src: url,
+							dest: 'dist/' + url
+						}
 					}
 				}
-			}
-		}, 
-		{
-			decodeEntities: true
-		});
+			},
+			{
+				decodeEntities: true
+			});
 		parser.write(fs.readFileSync('index.html', 'utf8'));
 		parser.end();
 
 		grunt.config.merge({
-			copy:copy
+			copy: copy
 		});
-		
+
 		grunt.config.merge({
-			uglify:uglify
+			uglify: uglify
 		});
-		
+
 		grunt.config.merge({
-			cssmin:cssmin
+			cssmin: cssmin
 		});
 	});
-	
-	grunt.registerTask('manifest', 'manifest', function()
-	{
+
+	grunt.registerTask('manifest', 'manifest', function () {
 		const fs = require('fs');
 		const path = 'dist/manifest.cache';
 		const baseDir = 'dist';
-		
+
 		let content = '';
-		
-		const readDir = function(dir)
-		{
-			fs.readdirSync(dir).forEach((item, index, array) =>
-			{
-				if(item !== "." && item !== "..")
-				{
+
+		const readDir = function (dir) {
+			fs.readdirSync(dir).forEach((item, index, array) => {
+				if (item !== "." && item !== "..") {
 					const path = dir + '/' + item;
 					const stats = fs.statSync(path);
-					if(stats.isDirectory())
-					{
+					if (stats.isDirectory()) {
 						readDir(path);
 					}
-					else
-					{
+					else {
 						content += path.replace(baseDir + '/', '') + '\n';
 					}
 				}
@@ -234,16 +231,17 @@ module.exports = function (grunt) {
 		content = 'CACHE MANIFEST\n';
 		content += '# ' + pkg.version + '\n';
 		content += 'CACHE:\n';
-		
+
 		readDir(baseDir);
-		
+
 		content += 'NETWORK:\n';
 		content += '*\n';
 		content += 'FALLBACK:\n';
-		
-		fs.writeFileSync(path, content);
-	});	
 
+		fs.writeFileSync(path, content);
+	});
+
+	grunt.registerTask('doc', ['jsdoc']);
 	grunt.registerTask('test', ['jshint']);
 	grunt.registerTask('theme', ['sass']);
 	grunt.registerTask('cache', ['manifest']);
