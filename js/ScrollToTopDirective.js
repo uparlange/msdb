e877e61fa021fe5ad20b5f65af989c5825ccaf74
parlangeu <ugo.parlange@cgi.com>
2016-12-23 17:21:04 +0100

    add animation

--- js/ScrollToTopDirective.js
+++ js/ScrollToTopDirective.js
@@ -12,9 +12,32 @@ function(AbstractDirective)
 				
 				this._element = ElementRef.nativeElement;
 				
+				this._scrollDuration = 500;
+				
 				this._onClickHandler = () =>
 				{
-					window.scrollTo(0,0);
+					const cosParameter = window.scrollY / 2;
+					
+					let scrollCount = 0;
+					let oldTimestamp = performance.now();
+					
+					const step = (newTimestamp) =>
+					{
+						scrollCount += Math.PI / (this._scrollDuration / (newTimestamp - oldTimestamp));
+						if (scrollCount >= Math.PI) 
+						{
+							window.scrollTo(0, 0);
+						}
+						if (window.scrollY === 0)
+						{
+							return;
+						}
+						window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(scrollCount)));
+						oldTimestamp = newTimestamp;
+						window.requestAnimationFrame(step);
+					}
+					
+					window.requestAnimationFrame(step);
 				};
 			}
 		],
