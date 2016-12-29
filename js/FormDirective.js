a0d1458dfbbe6aeb59e929e8a80945edd317a9ad
parlangeu <ugo.parlange@cgi.com>
2016-12-29 14:48:37 +0100

    Manipulate DOM with Renderer

--- js/FormDirective.js
+++ js/FormDirective.js
@@ -5,17 +5,18 @@ function(AbstractDirective)
 		selector: "form"
 	}).Class({
 		extends:AbstractDirective,
-		constructor: [ng.core.ElementRef, 
-			function FormDirective (ElementRef)
+		constructor: [ng.core.ElementRef, ng.core.Renderer,
+			function FormDirective (ElementRef, Renderer)
 			{
 				AbstractDirective.call(this);
 				
 				this._element = ElementRef.nativeElement;
+				this._renderer = Renderer;
 			}
 		],
 		onInit:function()
 		{
-			this._element.action = "javascript:void(0);";
+			this._renderer.setElementProperty(this._element, "action", "javascript:void(0);");
 		}
 	});
 });
