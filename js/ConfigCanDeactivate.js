4cb36a8ecac2595dde306da224414ed301824b1c
parlangeu <ugo.parlange@cgi.com>
2017-03-05 08:01:30 +0100

    Workaround for async Deactivate Guard

--- js/ConfigCanDeactivate.js
+++ js/ConfigCanDeactivate.js
@@ -9,49 +9,24 @@ function(AbstractClass, TranslateManager)
 				AbstractClass.call(this);
 
 				this._translateManager = TranslateManager;
-
-				// bug 4.0.0-rc.2
-				this._L10N_CONFIRM_QUIT = null;
-				this._translateManager.getValues(["L10N_CONFIRM_QUIT"]).subscribe((translations) =>
-				{
-					this._L10N_CONFIRM_QUIT = translations.L10N_CONFIRM_QUIT;
-				});
 			}
 		],
-		// bug 4.0.0-rc.2
 		canDeactivate:function(component)
 		{
-			if(component.model.hasChanges())
-			{
-				return window.confirm(this._L10N_CONFIRM_QUIT)
-			}
-			else
-			{
-				return true;
-			}
-		}
-		/*
-		canDeactivate:function(component)
-		{
-			const eventEmitter = new ng.core.EventEmitter();
-
-			setTimeout(() =>
+			return new Promise((resolve) => 
 			{
 				if(component.model.hasChanges())
 				{
 					this._translateManager.getValues(["L10N_CONFIRM_QUIT"]).subscribe((translations) =>
 					{
-						eventEmitter.emit(window.confirm(translations.L10N_CONFIRM_QUIT));
+						resolve(window.confirm(translations.L10N_CONFIRM_QUIT));
 					});
 				}
 				else
 				{
-					eventEmitter.emit(true);
+					resolve(true);
 				}
-			},0);
-			
-			return eventEmitter;
+			});
 		}
-		*/
 	});		
 });
