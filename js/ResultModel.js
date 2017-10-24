57fbb427fb5183d0b037f793e3e2184620d6838a
Ugo Parlange <uparlange@gmail.com>
2017-10-24 22:21:27 +0200

    Angular 5.0.0-rc.5 Optimizations

--- js/ResultModel.js
+++ js/ResultModel.js
@@ -4,11 +4,35 @@ define(["AppUtils", "AbstractModel", "MsdbService", "ConnectionManager"],
 			extends: AbstractModel,
 			constructor: function ResultModel(MsdbService, ConnectionManager, Title) {
 				AbstractModel.call(this, MsdbService, ConnectionManager, Title);
+				this.SYSTEM_DEVICE = "System / Device";
+				this.SYSTEM_BIOS = "System / BIOS";
+				this.showBios = false;
+				this.showDevice = false;
+				this.showClone = false;
 			},
 			parameters: [
 				[MsdbService], [ConnectionManager], [ng.platformBrowser.Title]
 			],
 			functions: {
+				onInit: function () {
+					switch (this.params.value) {
+						case this.SYSTEM_DEVICE:
+							this.showBios = false;
+							this.showDevice = true;
+							this.showClone = false;
+							break;
+						case this.SYSTEM_BIOS:
+							this.showBios = true;
+							this.showDevice = false;
+							this.showClone = false;
+							break;
+						default:
+							this.showBios = false;
+							this.showDevice = false;
+							this.showClone = true;
+							break;
+					}
+				},
 				onRefresh: function () {
 					this.data = this._getInitData();
 					this._msdbService.search(this.params.type, this.params.value).subscribe((data) => {
@@ -37,6 +61,14 @@ define(["AppUtils", "AbstractModel", "MsdbService", "ConnectionManager"],
 							this.data.count = 0;
 						}
 					});
+				},
+				canShowGame: function (game) {
+					return (
+						(game.category !== this.SYSTEM_DEVICE && game.category !== this.SYSTEM_BIOS && game.cloneof == null) ||
+						(game.cloneof != null && this.showClone) ||
+						(game.category === this.SYSTEM_DEVICE && this.showDevice) ||
+						(game.category === this.SYSTEM_BIOS && this.showBios)
+					)
 				},
 				getSearchLabel: function (type) {
 					return (type) ? "L10N_SEARCH_BY_" + type.toUpperCase() : "";
