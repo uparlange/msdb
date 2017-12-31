e44c69113476312f21f04f3f133c8a2fbdeed26c
Ugo Parlange <uparlange@gmail.com>
2017-12-31 10:19:17 +0100

    Manage event on panel click

--- js/TreeComponent.js
+++ js/TreeComponent.js
@@ -8,20 +8,25 @@ define(["AbstractComponent", "AppUtils"],
 				this.selectedItem = null;
 				this.selectedItemChange = new ng.core.EventEmitter();
 				this.onClick = new ng.core.EventEmitter();
+				this.onChange = new ng.core.EventEmitter();
 			},
 			annotations: [
 				new ng.core.Component(AppUtils.getComponentConfiguration("tree", {
 					inputs: ["provider", "selectedItem"],
-					outputs: ["onClick", "selectedItemChange"]
+					outputs: ["onToggle", "onClick", "onChange", "selectedItemChange"]
 				}))
 			],
 			functions: {
 				trackByLabel: function (index, item) {
 					return item ? item.label : undefined;
 				},
-				toggleNode: function (panelRef, item) {
+				toggleNode: function (panelHeaderRef, item) {
 					this.selectedItem = (item === this.selectedItem) ? null : item;
 					this.selectedItemChange.emit(this.selectedItem);
+					this.onChange.emit({
+						item: this.selectedItem,
+						headerRef: panelHeaderRef._element.nativeElement
+					});
 				},
 				selectNode: function (item) {
 					this.onClick.emit(item);
