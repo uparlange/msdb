6d7d313a87bf39c56fb8eb1a3988aa0b292c2065
Ugo Parlange <uparlange@gmail.com>
2018-07-31 23:17:29 +0200

    Material 6.4.2, optimizations

--- js/MdiIconComponent.js
+++ js/MdiIconComponent.js
@@ -6,16 +6,48 @@ class MdiIconComponent extends AbstractComponent {
     static get annotations() {
         return this.getAnnotations({
             selector: "mdi-icon",
-            inputs: ["name", "size"]
+            inputs: ["name", "size"],
+            host: {
+                "role": "img"
+            }
         });
     }
     static get parameters() {
-        return AppUtils.getParameters(AbstractClassHelper);
+        return AppUtils.getParameters(AbstractClassHelper, ng.core.ElementRef);
     }
-    constructor(AbstractClassHelper) {
+    constructor(AbstractClassHelper, ElementRef) {
         super(AbstractClassHelper);
-        this.name = "help";
+        this._element = ElementRef.nativeElement;
+        this.name = "";
         this.size = 24;
+        this.iconExists = false;
+    }
+    onInit() {
+        this._refreshIconExists();
+    }
+    onChanges(event) {
+        if (event.hasOwnProperty("name")) {
+            this._refreshIconExists();
+        }
+    }
+    _refreshIconExists() {
+        this.iconExists = this._iconExists();
+    }
+    _iconExists() {
+        const styleSheetsCount = document.styleSheets.length;
+        for (let i = 0; i < styleSheetsCount; i++) {
+            const styleSheet = document.styleSheets[i];
+            if (styleSheet.href && styleSheet.href.indexOf("@mdi") != -1) {
+                const rulesCount = styleSheet.rules.length;
+                for (let j = 0; j < rulesCount; j++) {
+                    const rule = styleSheet.rules[j];
+                    if (rule.selectorText == ".mdi-" + this.name + "::before") {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
 
